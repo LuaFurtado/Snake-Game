8781bf8dfd9b16dd998f06313a0092b4137bd26b
Luana Furtado <luana.furtado@calbrightcollege.org>
2025-11-27 00:14:24 -0800

    Added canvas and set up the color palette for the Snake Game.

--- snake.js
+++ snake.js
@@ -1,2 +1,6 @@
+const PINK_LIGHT = "#fce4ec";
+const PINK_HOT = "#ff0099";
+const GREEN_WICKED = "#1b5e20";
+
 const canvas = document.getElementById("snakeGame");
 const ctx = canvas.getContext("2d");
