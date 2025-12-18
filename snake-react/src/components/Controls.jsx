dd887010054f7b384479ebffc6e317b387ccf7e8
Luana Furtado <luana.furtado@calbrightcollege.org>
2025-12-17 21:51:12 -0800

    it looks very bad, but the assets were imported and rendered, no CSS needs to solve

--- snake-react/src/components/Controls.jsx
+++ snake-react/src/components/Controls.jsx
@@ -1,3 +1,8 @@
+import arrowUp from "../assets/arrow-up.png";
+import arrowDown from "../assets/arrow-down.png";
+import arrowLeft from "../assets/arrow-left.png";
+import arrowRight from "../assets/arrow-right.png";
+
 function Controls({ gameRef }) {
   const game = gameRef.current;
 
@@ -7,40 +12,48 @@ function Controls({ gameRef }) {
 
   return (
     <div>
-      <button onClick={() => {
-        if (snake.yVelocity !== 1) {
-          snake.yVelocity = -1;
-          snake.xVelocity = 0;
-        }
-      }}>
-        ↑
+      <button
+        onClick={() => {
+          if (snake.yVelocity !== 1) {
+            snake.yVelocity = -1;
+            snake.xVelocity = 0;
+          }
+        }}
+      >
+        <img src={arrowUp} alt="Move up" />
       </button>
 
-      <button onClick={() => {
-        if (snake.yVelocity !== -1) {
-          snake.yVelocity = 1;
-          snake.xVelocity = 0;
-        }
-      }}>
-        ↓
+      <button
+        onClick={() => {
+          if (snake.yVelocity !== -1) {
+            snake.yVelocity = 1;
+            snake.xVelocity = 0;
+          }
+        }}
+      >
+        <img src={arrowDown} alt="Move down" />
       </button>
 
-      <button onClick={() => {
-        if (snake.xVelocity !== 1) {
-          snake.xVelocity = -1;
-          snake.yVelocity = 0;
-        }
-      }}>
-        ←
+      <button
+        onClick={() => {
+          if (snake.xVelocity !== 1) {
+            snake.xVelocity = -1;
+            snake.yVelocity = 0;
+          }
+        }}
+      >
+        <img src={arrowLeft} alt="Move left" />
       </button>
 
-      <button onClick={() => {
-        if (snake.xVelocity !== -1) {
-          snake.xVelocity = 1;
-          snake.yVelocity = 0;
-        }
-      }}>
-        →
+      <button
+        onClick={() => {
+          if (snake.xVelocity !== -1) {
+            snake.xVelocity = 1;
+            snake.yVelocity = 0;
+          }
+        }}
+      >
+        <img src={arrowRight} alt="Move right" />
       </button>
     </div>
   );
