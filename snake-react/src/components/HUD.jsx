c37ddf841c6b56b817712e33ac248841e2350e8d
Luana Furtado <luana.furtado@calbrightcollege.org>
2025-12-18 12:46:38 -0800

    implement basic HUD structure

---
+++ snake-react/src/components/HUD.jsx
@@ -0,0 +1,11 @@
+//HUD (Heads-Up Display)
+//This component displays the score and game over message.
+function HUD() {
+  return (
+    <div className="hud">
+      <p>Level:</p>
+    </div>
+  );
+}
+
+export default HUD;
