3c7551321c8aa92564c643768272ced08b840f67
Luana Furtado <luana.furtado@calbrightcollege.org>
2025-12-18 22:42:14 -0800

    added comments abput the gameConfigs section

--- snake-react/src/gameConfigs.js
+++ snake-react/src/gameConfigs.js
@@ -1,4 +1,7 @@
 // Centralized game configuration for different modes
+// Got help from my friend Manu
+// Everything that changes between game modes goes here
+
 
 export const CLASSIC_CONFIG = {
   speed: 100,
